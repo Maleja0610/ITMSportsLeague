353eb9de2341d794b6704e49c41bd6091e6b8928
Maria Alejandra Lagos <maleja0089@gmail.com>
2026-03-30 15:45:58 -0500

    new entities: Referee, Tournament and TournamentTeam

--- SportsLeague.Domain/Entities/AuditBase.cs
+++ SportsLeague.Domain/Entities/AuditBase.cs
@@ -1,14 +1,9 @@
-﻿namespace SportsLeague.Domain.Entities;
-
-
-public abstract class AuditBase
-
+﻿namespace SportsLeague.Domain.Entities
 {
-
-    public int Id { get; set; }
-
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-    public DateTime? UpdatedAt { get; set; }
-
+    public abstract class AuditBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } //Notación Elvis
+    }
 }
--- SportsLeague.Domain/Enums/PlayerPosition.cs
+++ SportsLeague.Domain/Enums/PlayerPosition.cs
@@ -1,16 +1,10 @@
-﻿namespace SportsLeague.Domain.Enums;
-
-
-public enum PlayerPosition
-
+﻿namespace SportsLeague.Domain.Enums
 {
-
-    Goalkeeper = 0,
-
-    Defender = 1,
-
-    Midfielder = 2,
-
-    Forward = 3
-
+    public enum PlayerPosition
+    {
+        Goalkeeper = 0,
+        Defender = 1,
+        Midfielder = 2,
+        Forward = 3
+    }
 }
